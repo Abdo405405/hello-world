09a9ebab1b654f2b693c6dcdf5af7a6e2a17dce5
Ali <abdulrahman73098@gmail.com>
2021-11-03 07:37:38 +0200

    my name

---
+++ Project1/تجربة.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int num1 = 5;
+using namespace std;
+int main() {
+	cout << "please,type any number";
+	long g;
+	cout << "" << g;
+	cout << "Hi, nice to meet you" << endl;
+}
+	   
+	    
